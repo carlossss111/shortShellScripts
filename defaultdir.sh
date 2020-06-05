49e3aa16c14b915bd375466cb50ca4af840a2261
LinuxDaniel <danielrobinson1112@gmail.com>
2020-06-05 14:54:35 +0100

    Added default working directory script.

---
+++ defaultdir.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+###Code moved into the end of ~/.bashrc to change default working directory.
+###Script written by Daniel 06/20
+if [ -d "/home/daniel/Documents/repos" ]
+then
+	cd /home/daniel/Documents/repos		
+fi
